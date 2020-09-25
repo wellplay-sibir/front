a7079ae35c18a3d347dc819f702764cc08f8cf3f
clayzenx <clay.zenx@gmail.com>
2020-09-26 03:01:56 +0700

    end design loggin page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,16 @@
+<script>
+	import '../components/Login.svelte'
+	import Login from "../components/Login.svelte";
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+	:global(body) { background-color: #f5f5f5}
+	.background {background-color: #F5F5F5; width: 100%; height: 100vh}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>bank login</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<Login/>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
